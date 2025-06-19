d3117cf6036177e2a3a7f7728795313c8487e284
timzaak <zsy.evan@gmail.com>
2025-06-19 11:48:46 +0800

    add env params

--- src/main/scala/RunRemoteScript.scala
+++ src/main/scala/RunRemoteScript.scala
@@ -3,6 +3,10 @@ import com.timzaak.devops.scripts.ConfigShellRun
 object RunRemoteScript {
 
   def main(args: Array[String]): Unit = {
-    ConfigShellRun.remoteRun(args(0))
+    val env = args.tail.map { v =>
+      val Array(key, value) = v.split("=", 2)
+      key -> value
+    }.toMap
+    ConfigShellRun.remoteRun(args(0), env)
   }
 }
